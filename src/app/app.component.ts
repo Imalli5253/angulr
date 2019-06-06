9a6e9175e439d42a482bd9355789af952337b186
Imalli5253 <pnr.759@gmail.com>
2019-06-06 05:30:15 +0000

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements OnInit {
+  title = 'Chart';
+  LineChart=[];
+  ngOnInit()
+  {
+    this.LineChart = new Chart('lineChart', {
+      type: 'line',
+    data: {
+     labels: ["Jan", "Feb", "March", "April", "May", "June","July","Aug","Sep","Oct","Nov","Dec"],
+     datasets: [{
+         label: 'Number of Items Sold in Months',
+         data: [9,7 , 3, 5, 2, 10,15,16,19,3,1,9],
+         fill:false,
+         lineTension:0.2,
+         borderColor:"red",
+         borderWidth: 1
+     }]
+    }, 
+    options: {
+     title:{
+         text:"Line Chart",
+         display:true
+     },
+     scales: {
+         yAxes: [{
+             ticks: {
+                 beginAtZero:true
+             }
+         }]
+     }
+    }
+    });
+    
+   
+  }
+
+  
 }
